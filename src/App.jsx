9f7652a72e5ae9fa2d4133cbb5fba9eeb50625b2
Sebastian Asprino <asprinosebastian@proton.me>
2024-03-28 00:51:43 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ComponentePotencia from './componets/potencia_1'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <ComponentePotencia/>
+    </div>
+  );
+}
+export default App
---
+++ src/componets/potencia.js
@@ -0,0 +1,6 @@
+// potencia.js
+function calcularPotencia(numero) {
+  return Math.pow(numero, numero);
+}
+
+export default calcularPotencia;
